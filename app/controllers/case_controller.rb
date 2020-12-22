6b579d8b423eca71a83dff025a5f936ff1f029a1
AnaBetancourt <anaa.betancourt13@gmail.com>
2020-12-21 23:45:35 -0500

    defined RESTFUL routes for CRUD methods

--- app/controllers/case_controller.rb
+++ app/controllers/case_controller.rb
@@ -1,5 +1,32 @@
 class CaseController < ApplicationController
 
+    get '/cases' do
+       erb :"/cases/index" 
+    end
+
+    get '/cases/new' do
+        erb :"/cases/new"
+    end
+
+    post '/cases' do
+        
+    end
+
+    get '/cases/:id' do
+        erb :"/cases/show"
+    end
+
+    get '/cases/:id/edit' do
+        erb :"/cases/edit"
+    end
+
+    patch '/cases/:id' do
+        
+    end
+
+    delete '/cases/:id' do
+        
+    end
 
 
 end
