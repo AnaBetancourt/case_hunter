f9b5a1d1049d069ec7af60304294bb63b399cf8d
AnaBetancourt <anaa.betancourt13@gmail.com>
2020-12-28 19:39:01 -0500

    Outlined controller actions for hunters

--- app/controllers/hunter_controller.rb
+++ app/controllers/hunter_controller.rb
@@ -1,5 +1,41 @@
 class HunterController < ApplicationController
+    get '/hunters' do
+        erb :'/hunters/index'
+    end
+ 
+    get '/hunters/new' do
+ 
+        erb :"/hunters/new"
+    end
+ 
+    post '/hunters' do
+        @hunter = 
+        redirect "/hunters/#{@hunter.id}"
+    end
+ 
+    get '/hunters/:id' do
+        @hunter = Hunter.find_by(id: params[:id])
 
-
-
+        erb :"/hunters/show"
+    end
+ 
+    get '/hunters/:id/edit' do
+        @hunter = Hunter.find_by(id: params[:id])
+     
+        erb :"/hunters/edit"
+    end
+ 
+    patch '/hunters/:id' do
+        @hunter = Hunter.find_by(id: params[:id])
+ 
+         
+        redirect "/hunters/#{@hunter.id}"
+    end
+ 
+    delete '/hunters/:id' do
+        @hunter = Hunter.find_by(id: params[:id])
+ 
+        redirect "/hunters"
+    end
+ 
 end
