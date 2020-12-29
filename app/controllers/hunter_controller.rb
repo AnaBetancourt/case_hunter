ba208abb53638ca9e3e84a5728c382820fcebbf3
AnaBetancourt <anaa.betancourt13@gmail.com>
2020-12-28 20:02:57 -0500

    Created signup page

--- app/controllers/hunter_controller.rb
+++ app/controllers/hunter_controller.rb
@@ -1,15 +1,12 @@
 class HunterController < ApplicationController
-    get '/hunters' do
-        erb :'/hunters/index'
-    end
  
     get '/hunters/new' do
- 
         erb :"/hunters/new"
     end
  
     post '/hunters' do
-        @hunter = 
+        @hunter = Hunter.create(params)
+
         redirect "/hunters/#{@hunter.id}"
     end
  
