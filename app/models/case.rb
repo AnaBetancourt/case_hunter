8a2fc872a4abe925f3ce0c395650a6b098d00bb3
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-01-03 19:02:28 -0500

    Added validations to Case class

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,4 +1,9 @@
 class Case < ActiveRecord::Base
     belongs_to :hunter
     belongs_to :monster
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :location, presence: true
+    validates :description, presence: true
+    validates :monster_id, presence: true
 end
